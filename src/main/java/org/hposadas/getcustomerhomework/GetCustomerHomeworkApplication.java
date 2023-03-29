56fe511e40af625a60c96bec79939f4aa9006beb
Humberto Posadas <posadas.humberto@outlook.com>
2023-03-29 15:36:54 +0200

    Creating project structure

---
+++ src/main/java/org/hposadas/getcustomerhomework/GetCustomerHomeworkApplication.java
@@ -0,0 +1,13 @@
+package org.hposadas.getcustomerhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetCustomerHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetCustomerHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hposadas/getcustomerhomework/GetCustomerHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hposadas.getcustomerhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetCustomerHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
